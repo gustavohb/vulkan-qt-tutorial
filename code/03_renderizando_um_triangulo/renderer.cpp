cf45a264742c8b7ec75353513a758e518c5de3e4
Gustavo Barrionuevo <gustavohb@gmail.com>
2020-06-18 16:58:22 -0300

    Criação do shader stage

--- code/03_renderizando_um_triangulo/renderer.cpp
+++ code/03_renderizando_um_triangulo/renderer.cpp
@@ -22,11 +22,26 @@ void Renderer::startNextFrame() {
 }
 
 void Renderer::initPipeline() {
+    VkDevice device = m_window->device();
     QByteArray vertShaderCode = readFile(":shaders/shader.vert.spv");
     QByteArray fragShaderCode = readFile(":shaders/shader.frag.spv");
 
     VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
     VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
 
     m_deviceFunctions->vkDestroyShaderModule(device, fragShaderModule, nullptr);
     m_deviceFunctions->vkDestroyShaderModule(device, vertShaderModule, nullptr);
