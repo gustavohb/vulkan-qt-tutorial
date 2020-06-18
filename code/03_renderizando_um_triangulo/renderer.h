81d92ad42f0d0a7d85e5e6a40419cb28ad25f1e2
Gustavo Barrionuevo <gustavohb@gmail.com>
2020-06-18 16:40:55 -0300

    Renderizando um triângulo

---
+++ code/03_renderizando_um_triangulo/renderer.h
@@ -0,0 +1,22 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <QVulkanWindowRenderer>
+#include <QVulkanDeviceFunctions>
+
+class VulkanWindow;
+
+class Renderer : public QVulkanWindowRenderer {
+public:
+    Renderer(VulkanWindow *window);
+
+    void initResources() override;
+    void startNextFrame() override;
+
+private:
+
+    VulkanWindow *m_window = nullptr;
+    QVulkanDeviceFunctions *m_deviceFunctions;
+};
+
+#endif // RENDERER_H
