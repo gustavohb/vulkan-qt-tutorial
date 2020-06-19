fa3a0117278a83ae071846a85ed0c1ac4c84c252
Gustavo Barrionuevo <gustavohb@gmail.com>
2020-06-19 11:20:08 -0300

    Descrições de atributos

--- code/04_usando_buffer_de_vertice/model.h
+++ code/04_usando_buffer_de_vertice/model.h
@@ -4,6 +4,7 @@
 #include <QVector2D>
 #include <QVector3D>
 #include <QVector>
+#include <array>
 
 struct Vertex {
     QVector2D pos;
@@ -17,6 +18,22 @@ struct Vertex {
         bindingDescription.binding = 0;
 
         return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions = {};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+        return attributeDescriptions;
     }
 };
 
