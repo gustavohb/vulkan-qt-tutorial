ca0b4bd5fae8065d2177896db8a799de3b6c1b88
Gustavo Barrionuevo <gustavohb@gmail.com>
2020-06-19 15:16:20 -0300

    Coordenadas de textura

--- code/05_usando_texturas_em_shaders/model.h
+++ code/05_usando_texturas_em_shaders/model.h
@@ -9,6 +9,7 @@
 struct Vertex {
     QVector2D pos;
     QVector3D color;
+    QVector2D texCoord;
 
     static VkVertexInputBindingDescription getBindingDescription() {
         VkVertexInputBindingDescription bindingDescription = {};
@@ -20,8 +21,8 @@ struct Vertex {
         return bindingDescription;
     }
 
-    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions = {};
+    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions = {};
 
         attributeDescriptions[0].binding = 0;
         attributeDescriptions[0].location = 0;
@@ -33,6 +34,11 @@ struct Vertex {
         attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[1].offset = offsetof(Vertex, color);
 
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
+
         return attributeDescriptions;
     }
 };
@@ -42,9 +48,12 @@ struct Model
     Model() {};
 
     QVector<Vertex> vertices = {
-           { {0.0f, -0.5f}, {1.0f, 0.0f, 0.0f} },
-           { {0.5f,  0.5f}, {0.0f, 1.0f, 0.0f} },
-           { {-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f} }
+        { {-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f} },
+        { { 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f} },
+        { { 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f} },
+        { { 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f} },
+        { {-0.5f,  0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f} },
+        { {-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f} }
     };
 };
 
